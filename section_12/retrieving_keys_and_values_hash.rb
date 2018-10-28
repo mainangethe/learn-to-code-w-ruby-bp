f29d6363cb33e3f0f5a25d0a9efe72021af8b02f
mainangethe <ngethe@mradi.co.ke>
2018-10-28 12:51:00 +0300

    28/10 Completed section 12 (3 methods)

---
+++ section_12/retrieving_keys_and_values_hash.rb
@@ -0,0 +1,14 @@
+# retrieving all the keys / values from a hash
+# always returns an array
+shopping_list = {
+                bananas: 5,
+                oranges: 10,
+                carrots: 3,
+                crackers: 3
+                }
+
+# keys method
+p shopping_list.keys
+
+# values method
+p shopping_list.values
